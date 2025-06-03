5c66098b117bf9e738357b816a1d0959abd5d355
Cleytonrik99 <enriquecleyton@gmail.com>
2025-06-03 14:31:36 -0300

    mudanca if name

--- app.py
+++ app.py
@@ -419,6 +419,6 @@ def visualizar_pedidos():
 
 app.run(debug=True)
 
-if __name__ == "main":
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
